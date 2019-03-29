c968a6fafe16661c790cb8df5a405820763ffe53
Janne Mäkelä <jarppe.makela@gmail.com>
2019-03-29 12:13:57 +0200

    lisää harjoittelua

--- projects/round1/osa1/src/index.js
+++ projects/round1/osa1/src/index.js
@@ -1,31 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const Hello = ({ name, age }) => {
+const Display = ({ counter }) => {
+  console.log('renderDisplay')
+  return (<div>{counter}</div>)
+}
 
-  const bornYear = () => new Date().getFullYear() - age
+const Button = ({ handleClick, text }) => {
+  console.log('renderButton')
+  return (<button onClick={handleClick}>
+    {text}
+  </button>)
+}
+
+const App = (_props) => {
+  const [ counter, setCounter ] = useState(0)
+
+  const setToValue = (value) => setCounter(value)
+
+/*
+  setTimeout(
+    () => setCounter(counter + 1),
+    1000
+  )
+*/
 
   return (
     <div>
-      <p>
-        Hello {name}, you are {age} years old
-      </p>
-      <p>So you were probably born {bornYear()}</p>
+      <Display counter={counter}/>
+      <Button
+        handleClick={() => setToValue(counter + 1)}
+        text='plus'
+      />
+      <Button
+        handleClick={() => setToValue(counter - 1)}
+        text='minus'
+      />
+      <Button
+        handleClick={() => setToValue(0)}
+        text='zero'
+      />
     </div>
   )
 }
 
-const App = () => {
-  const nimi = 'Pekka'
-  const ika = 10
-
-  return (
-    <div>
-      <h1>Greetings</h1>
-      <Hello name="Arto" age={26 + 10} />
-      <Hello name={nimi} age={ika} />
-    </div>
-  )
-}
-
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <App />, 
+  document.getElementById('root')
+)
