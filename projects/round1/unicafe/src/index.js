1fd60aa9a6dcab7b304266f727fa104043f202e3
Janne Mäkelä <jarppe.makela@gmail.com>
2019-04-04 17:56:16 +0300

    1.6: unicafe step1

--- projects/round1/unicafe/src/index.js
+++ projects/round1/unicafe/src/index.js
@@ -1,12 +1,69 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Header = ({name}) => {
+    return <h1>{name}</h1>
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const Button = ({ handleClick, text }) => {
+    console.log('renderButton')
+    return (<button onClick={handleClick}>
+      {text}
+    </button>)
+  }
+
+const Content = (props) => {
+    return (
+        <div>
+          <Part label={props.labels.good} score={props.goodScore} />
+          <Part label={props.labels.neutral} score={props.neutralScore} />
+          <Part label={props.labels.bad} score={props.badScore} />
+        </div>
+      )
+}
+
+const Part = (props) => {
+    return <p>{props.label} {props.score}</p>
+}
+
+const App = () => {
+
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const firstHeader = 'anna palautetta'
+  const secondHeader = 'statistiikka'
+  const scoreLabels =
+  {
+    good: 'hyvä',
+    neutral: 'neutraali',
+    bad: 'huono'
+  }
+
+  return (
+    <div>
+      <Header name={firstHeader} />
+      <Button
+        handleClick={() => setGood(good + 1)}
+        text={scoreLabels.good} />
+      <Button
+        handleClick={() => setNeutral(neutral + 1)}
+        text={scoreLabels.neutral} />
+      <Button
+        handleClick={() => setBad(bad + 1)}
+        text={scoreLabels.bad} />
+      <Header name={secondHeader} />
+      <Content
+        labels={scoreLabels}
+        goodScore={good}
+        neutralScore={neutral}
+        badScore={bad} />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, 
+  document.getElementById('root')
+)
