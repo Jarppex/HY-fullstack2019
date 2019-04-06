c8b663143c97bdc7618888431324b88df1f7cb51
Janne Mäkelä <jarppe.makela@gmail.com>
2019-04-06 15:46:19 +0300

    2.1: kurssitiedot step6

--- projects/round2/kurssitiedot/src/index.js
+++ projects/round2/kurssitiedot/src/index.js
@@ -1,51 +1,59 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const Header = props =>
-  <h1>{props.course}</h1>
-
+/*
 const Total = props => {
   const total = props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises
 
   return <p>yhteensä {total} tehtävää</p>
 }
-  
+//<Total parts={course.parts} />
+*/
 
 const Part = props =>
-  <p>{props.part.name} {props.part.exercises}</p>
+    <p>{props.part.name} {props.part.exercises}</p>
 
-const Content = props => (
-  <div>
-    <Part part={props.parts[0]} />
-    <Part part={props.parts[1]} />
-    <Part part={props.parts[2]} />
-  </div>
-)
+const Header = props =>
+    <h1>{props.course}</h1>
+
+const Course = ({course}) => {
+
+    const parts = () => course.parts.map(part => 
+        <Part key={part.id} part={part} />)
+
+    return (
+    <div>
+        <Header course={course.name} />
+        {parts()}
+    </div>
+    )
+}
 
 const App = () => {
-  const course = {
-    name: 'Half Stack -sovelluskehitys',
-    parts: [
-      {
-        name: 'Reactin perusteet',
-        exercises: 10
-      },
-      {
-        name: 'Tiedonvälitys propseilla',
-        exercises: 7
-      },
-      {
-        name: 'Komponenttien tila',
-        exercises: 14
+    const course = {
+        name: 'Half Stack -sovelluskehitys',
+        parts: [
+          {
+            name: 'Reactin perusteet',
+            exercises: 10,
+            id: 1
+          },
+          {
+            name: 'Tiedonvälitys propseilla',
+            exercises: 7,
+            id: 2
+          },
+          {
+            name: 'Komponenttien tila',
+            exercises: 14,
+            id: 3
+          }
+        ]
       }
-    ]
-  } 
 
   return (
     <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
+      <Course course={course} />
     </div>
   )
 }
