0f032518fabf743cf762780992c7d4b14a2799ea
Janne Mäkelä <jarppe.makela@gmail.com>
2019-04-06 16:23:42 +0300

    2.4: kurssitiedot step9

--- projects/round2/kurssitiedot/src/index.js
+++ projects/round2/kurssitiedot/src/index.js
@@ -16,50 +16,67 @@ const Header = props =>
 
 const Course = ({course}) => {
 
-    const parts = () => course.parts.map(part => 
+    const allParts = () => course.parts.map(part => 
         <Part key={part.id} part={part} />)
 
     return (
     <div>
         <Header course={course.name} />
-        {parts()}
+        {allParts()}
         <Total parts={course.parts} />
     </div>
     )
 }
 
 const App = () => {
-    const course = {
-        name: 'Half Stack -sovelluskehitys',
-        parts: [
-          {
-            name: 'Reactin perusteet',
-            exercises: 10,
-            id: 1
-          },
-          {
-            name: 'Tiedonvälitys propseilla',
-            exercises: 7,
-            id: 2
-          },
-          {
-            name: 'Komponenttien tila',
-            exercises: 14,
-            id: 3
-          },
-          {
-            name: 'Redux',
-            exercises: 7,
-            id: 4
-          }
-        ]
-      }
+    const courses = [
+        {
+          name: 'Half Stack -sovelluskehitys',
+          id: 1,
+          parts: [
+            {
+              name: 'Reactin perusteet',
+              exercises: 10,
+              id: 1
+            },
+            {
+              name: 'Tiedonvälitys propseilla',
+              exercises: 7,
+              id: 2
+            },
+            {
+              name: 'Komponenttien tila',
+              exercises: 14,
+              id: 3
+            }
+          ]
+        },
+        {
+          name: 'Node.js',
+          id: 2,
+          parts: [
+            {
+              name: 'Routing',
+              exercises: 3,
+              id: 1
+            },
+            {
+              name: 'Middlewaret',
+              exercises: 7,
+              id: 2
+            }
+          ]
+        }
+    ]
 
-  return (
-    <div>
-      <Course course={course} />
-    </div>
-  )
+    const allCourses = () => courses.map(course =>
+        <Course key={course.id} course={course} />)
+
+    return (
+        <div>
+            {allCourses()}
+        </div>
+    )
 }
 
 ReactDOM.render(
