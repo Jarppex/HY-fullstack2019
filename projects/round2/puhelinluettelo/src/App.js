07bfd132785b90e83b8be322abef5cfb930ee466
Janne Mäkelä <jarppe.makela@gmail.com>
2019-04-07 18:38:31 +0300

    2.8: puhelinluettelo step3

--- projects/round2/puhelinluettelo/src/App.js
+++ projects/round2/puhelinluettelo/src/App.js
@@ -1,34 +1,54 @@
 import React, { useState } from 'react'
 
+
 const App = () => {
+
   const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas',
+      number: '045-123456' }
   ]) 
   const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
 
-  const handleNameChange = (event) => {
-    console.log(event.target.value)
-    setNewName(event.target.value)
-  }
+  const handleNameChange = (event) => setNewName(event.target.value)
+  const handleNumberChange = (event) => setNewNumber(event.target.value)
 
   const sameElement = (element) => element.name === newName
 
   const addPerson = (event) => {
     event.preventDefault()
-    if (persons.find(sameElement) === undefined)
+    if (newName !== '')
     {
-      const personObject = { name: newName }
-      setPersons(persons.concat(personObject))
-      setNewName('')
+      if (persons.find(sameElement) === undefined)
+      {
+        const personObject = { name: newName, number: newNumber }
+        setPersons(persons.concat(personObject))
+      }
+      else
+      {
+        if (newNumber !== '')
+        {
+          const copyPersons = persons.map(element => element)
+          const samePerson = copyPersons.find(sameElement)
+          samePerson.number = newNumber
+          setPersons(copyPersons)
+        }
+        else
+        {
+          window.alert(`${newName} on jo luettelossa`)
+        }
+      }
     }
     else
     {
-      window.alert(`${newName} on jo luettelossa`)
+      window.alert(`nimi-kenttä on tyhjä`)
     }
+    setNewName('')
+    setNewNumber('')
   }
 
   const allPersons = () => persons.map( person =>
-        <p key={person.name}>{person.name}</p> )
+        <p key={person.name}>{person.name} {person.number}</p> )
 
   return (
     <div>
@@ -41,6 +61,12 @@ const App = () => {
           />
         </div>
         <div>
+          numero: <input
+          value={newNumber}
+          onChange={handleNumberChange}
+          />
+        </div>
+        <div>
           <button type="submit">lisää</button>
         </div>
       </form>
@@ -48,7 +74,6 @@ const App = () => {
       {allPersons()}
     </div>
   )
-
 }
 
 export default App
