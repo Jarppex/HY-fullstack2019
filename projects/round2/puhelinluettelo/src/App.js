0e66bd833678a4510b43e92c4be0efb54e76f7cb
Janne Mäkelä <jarppe.makela@gmail.com>
2019-04-07 19:19:22 +0300

    2.9*: puhelinluettelo step4

--- projects/round2/puhelinluettelo/src/App.js
+++ projects/round2/puhelinluettelo/src/App.js
@@ -3,15 +3,19 @@ import React, { useState } from 'react'
 
 const App = () => {
 
-  const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas',
-      number: '045-123456' }
-  ]) 
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Martti Tienari', number: '040-123456' },
+    { name: 'Arto Järvinen', number: '040-123456' },
+    { name: 'Lea Kutvonen', number: '040-123456' }
+  ])
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
+  const [ limit, setLimit ] = useState('')
 
   const handleNameChange = (event) => setNewName(event.target.value)
   const handleNumberChange = (event) => setNewNumber(event.target.value)
+  const handleLimitChange = (event) => setLimit(event.target.value)
 
   const sameElement = (element) => element.name === newName
 
@@ -47,12 +51,27 @@ const App = () => {
     setNewNumber('')
   }
 
-  const allPersons = () => persons.map( person =>
-        <p key={person.name}>{person.name} {person.number}</p> )
+  const matchFilter = (element) =>
+    element.name.toUpperCase().startsWith(limit.toUpperCase())
+
+  const showPersons = () => {
+    const personsToShow = persons.filter(matchFilter)
+    return (
+      personsToShow.map(person => <p key={person.name}>{person.name} {person.number}</p>)
+    )
+  }
+        
 
   return (
     <div>
       <h2>Puhelinluettelo</h2>
+      <div>
+        rajaa näytettäviä <input
+        value={limit}
+        onChange={handleLimitChange}
+        />
+      </div>
+      <h3>lisää uusi</h3>
       <form onSubmit={addPerson}>
         <div>
           nimi: <input
@@ -70,8 +89,8 @@ const App = () => {
           <button type="submit">lisää</button>
         </div>
       </form>
-      <h2>Numerot</h2>
-      {allPersons()}
+      <h3>Numerot</h3>
+      {showPersons()}
     </div>
   )
 }
