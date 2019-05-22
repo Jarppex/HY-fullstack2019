3401f5a6ae09fa1ef550f232e7bdcfec715b7998
Janne Mäkelä <jarppe.makela@gmail.com>
2019-05-22 12:20:25 +0300

    5.1: blogilistan frontend, step1

--- projects/round5/bloglist-frontend/src/App.js
+++ projects/round5/bloglist-frontend/src/App.js
@@ -1,9 +1,13 @@
 import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('') 
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,8 +15,59 @@ const App = () => {
     )  
   }, [])
 
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    console.log('logging in with', username, password)
+    try {
+      const user = await loginService.login({
+        username, password,
+      })
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      console.log('käyttäjätunnus tai salasana virheellinen')
+      /*
+      setErrorMessage('käyttäjätunnus tai salasana virheellinen')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+      */
+    }
+  }
+
+  if (user === null) {
+    return (
+      <div>
+        <h2>Log in to application</h2>
+        <form onSubmit={handleLogin}>
+        <div>
+          käyttäjätunnus
+            <input
+            type="text"
+            value={username}
+            name="Username"
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          salasana
+            <input
+            type="password"
+            value={password}
+            name="Password"
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <button type="submit">kirjaudu</button>
+        </form>
+      </div>
+    )
+  }
+
   return (
     <div>
+      <p>{user.name} logged in</p>
       <h2>blogs</h2>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
