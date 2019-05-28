9cb633f4bceaa1e5fc5adc6dd23aa8c7d02140b2
Janne Mäkelä <jarppe.makela@gmail.com>
2019-05-28 14:43:44 +0300

    5.14: blogilistan testit, step2

--- projects/round5/bloglist-frontend/src/components/SimpleBlog.test.js
+++ projects/round5/bloglist-frontend/src/components/SimpleBlog.test.js
@@ -1,17 +1,17 @@
 import React from 'react'
 import 'jest-dom/extend-expect'
-import { render, cleanup } from 'react-testing-library'
+import { render, cleanup, fireEvent } from 'react-testing-library'
 import SimpleBlog from './SimpleBlog'
 
 afterEach(cleanup)
 
-test('renders content', () => {
-  const blog = {
-    title: 'Kirjoitelmia elämästä',
-    author: 'Helena',
-    likes: 3
-  }
+const blog = {
+  title: 'Kirjoitelmia elämästä',
+  author: 'Helena',
+  likes: 3
+}
 
+test('renders content', () => {
   const component = render(
     <SimpleBlog blog={blog} />
   )
@@ -20,4 +20,18 @@ test('renders content', () => {
     'Kirjoitelmia elämästä')
   expect(component.container).toHaveTextContent('Helena')
   expect(component.container).toHaveTextContent(3)
+})
+
+it('clicking the button calls event handler once', async () => {
+  const mockHandler = jest.fn()
+
+  const component = render(
+    <SimpleBlog blog={blog} onClick={mockHandler} />
+  )
+
+  const button = component.container.querySelector('.button')
+  fireEvent.click(button)
+  fireEvent.click(button)
+
+  expect(mockHandler.mock.calls.length).toBe(2)
 })
